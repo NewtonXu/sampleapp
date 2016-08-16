6e2e541ccc8b683537b121ba55d42c30f6d2e256
Newton Xu <newton.xu@hotmail.com>
2016-08-16 09:30:30 +0000

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815070024) do
+ActiveRecord::Schema.define(version: 20160817070024) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
